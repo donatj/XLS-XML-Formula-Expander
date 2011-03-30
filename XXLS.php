3effcceaf25e5aa3d5f8d53009476e13d304f3df
Jesse Donat <donatj@gmail.com>
2011-03-30 12:28:29 -0500

    = -> == regex updated to ignore <= and >=

--- XXLS.php
+++ XXLS.php
@@ -328,7 +328,7 @@ class XXLS {
 
 		//Functions
 		$expanded_formula = preg_replace('/([A-Z]{1,})\(/six', ' XXLS_METHODS::X_\1 ( ', $expanded_formula);
-		$expanded_formula = preg_replace('/(?<![=])=(?![=])/six', '==', $expanded_formula);
+		$expanded_formula = preg_replace('/(?<![<>=])=(?![=])/six', '==', $expanded_formula);
 
 		//Power Expansion
 		$expanded_formula .= ' '; //lazy fix for overflow issue.
