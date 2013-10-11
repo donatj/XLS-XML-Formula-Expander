ceae2d4f7575bd81f605f317ada474a3f41d9bea
Jesse Donat <donatj@gmail.com>
2013-10-11 17:54:57 -0500

    unit test cleanup

--- test/XXLSTest.php
+++ test/XXLSTest.php
@@ -4,34 +4,22 @@ class XXLSTest extends PHPUnit_Framework_TestCase {
 
 	public function testMatch() {
 
-		ini_set('memory_limit', '2048M');
+//		ini_set('memory_limit', '2048M');
 
 		$xls = new XXLS(__DIR__ . '/ss_test.xml');
-//		$xls->debug = true;
 
-		$sheet_data = $xls->getSheetData();
+		$sheet_data  = $xls->getSheetData();
 
-
-//		$testData = $xls->celltest( 'Inputs', 1, 56 );
-//		$this->assertTrue( $testData['passing'],  "\n--\n--\n--\n" . var_export($testData, true) );
-
-
-
-
-
-
-		$n = 0;
 		foreach( $sheet_data as $sheet => $cols ) {
 			foreach( $cols as $col => $rows ) {
 				foreach( $rows as $row => $data ) {
-					if( $data['formula'] ) {
+					if( isset($data['formula']) && $data['formula'] ) {
 						$testData = $xls->celltest($sheet, $row, $col);
-						$this->assertTrue( $testData['passing'], var_export($testData, true) );
+						$this->assertTrue($testData['passing'], var_export($testData, true));
 					}
 				}
 			}
 		}
-
 
 
 	}
