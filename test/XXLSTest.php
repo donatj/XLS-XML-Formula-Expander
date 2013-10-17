dfae5c5d0bb6d7e8fa93cdb8b7f9f325b0611ae5
Jesse Donat <donatj@gmail.com>
2013-10-17 12:19:10 -0500

    unit test scope expanded, cleanup

--- test/XXLSTest.php
+++ test/XXLSTest.php
@@ -8,31 +8,29 @@ class XXLSTest extends PHPUnit_Framework_TestCase {
 
 		$xls = new XXLS(__DIR__ . '/ss_test.xml');
 
-		$sheet_data  = $xls->getSheetData();
+		$sheet_data = $xls->getSheetData();
 
 		foreach( $sheet_data as $sheet => $cols ) {
 			foreach( $cols as $col => $rows ) {
 				foreach( $rows as $row => $data ) {
-					if( isset($data['formula']) && $data['formula'] ) {
-						$testData = $xls->celltest($sheet, $row, $col);
-						$this->assertTrue($testData['passing'], var_export($testData, true));
-					}
+					$testData = $xls->celltest($sheet, $row, $col);
+					$this->assertTrue($testData['passing'], var_export($testData, true));
 				}
 			}
 		}
 
 
- 	}
+	}
 
 	public function testBase_xls_rev() {
 
-		$this->assertEquals( 1, XXLS::base_xls_rev('A') );
-		$this->assertEquals( 4, XXLS::base_xls_rev('D') );
-		$this->assertEquals( 4, XXLS::base_xls_rev('d') );
-		$this->assertEquals( 26, XXLS::base_xls_rev('Z') );
-		$this->assertEquals( 27, XXLS::base_xls_rev('AA') );
+		$this->assertEquals(1, XXLS::base_xls_rev('A'));
+		$this->assertEquals(4, XXLS::base_xls_rev('D'));
+		$this->assertEquals(4, XXLS::base_xls_rev('d'));
+		$this->assertEquals(26, XXLS::base_xls_rev('Z'));
+		$this->assertEquals(27, XXLS::base_xls_rev('AA'));
 		$this->assertEquals(205, XXLS::base_xls_rev('GW'));
-		$this->assertEquals( 4670983, XXLS::base_xls_rev('JESSE') );
+		$this->assertEquals(4670983, XXLS::base_xls_rev('JESSE'));
 
 	}
 
